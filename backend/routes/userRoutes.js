c003eb78818846b08582c595952f4cab6263319a
RinkiShara <rinki2001sharma@gmail.com>
2025-06-27 01:07:43 +0530

    routes, controller

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const {registerUser,loginUser} = require("../controllers/userController");
+
+// Register new user
+router.post("/registrate", registerUser);
+
+// Login user
+router.get("/login", loginUser);
+
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB   
+connectDB();
+// Middleware   
+app.use(cors());
+app.use(express.json());
+// Routes
+app.use('/api', userRoutes);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
